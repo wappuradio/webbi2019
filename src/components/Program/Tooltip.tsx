b0f918f4bbf7f49a2c276a1c83ff6eb22bd320be
petri.sarasvirta <petri.sarasvirta@gofore.com>
2019-04-08 23:44:18 +0300

    added tooltip to the program map

---
+++ src/components/Program/Tooltip.tsx
@@ -0,0 +1,17 @@
+import React, { FunctionComponent, ReactNode } from 'react';
+
+interface TooltipProps {
+  tooltip: string,
+  children: ReactNode
+}
+
+const ToolTip: FunctionComponent<{tooltip?: string, children:ReactNode}> = ({ tooltip, children }) => (
+  <div className="map-tooltip">
+    {children}
+    <div className="map-tooltip-tooltiptext">
+      {tooltip}
+    </div>
+  </div>
+);
+
+export default ToolTip;
