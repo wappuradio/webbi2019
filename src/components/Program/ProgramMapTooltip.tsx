e3c607c0380a7313f853bed0ab7cb858cc49922d
petri.sarasvirta <petri.sarasvirta@gofore.com>
2019-04-09 01:26:40 +0300

    fixed tooltip and clipping

--- src/components/Program/ProgramMapTooltip.tsx
+++ src/components/Program/ProgramMapTooltip.tsx
@@ -8,13 +8,15 @@ interface TooltipProps {
 }
 
 const ProgramMapTooltip: FunctionComponent<{ program: Program, children: ReactNode }> = ({ program, children }) => (
-  <div className="map-tooltip">
-    {children}
+  <>
+  {children}
+  <div className="map-tooltip">    
     <div className="map-tooltip-content">
      <ProgramSingleItem {...program} showImg={false} />
     </div>
     
   </div>
+  </>
 );
 
 export default ProgramMapTooltip;
