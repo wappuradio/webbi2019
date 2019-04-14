5b7ef9b53c805ba3107a3038bd7501ae2b53560c
Noora Pirttilahti <noorapirttilahti@Nooras-MacBook-Pro.local>
2019-04-15 00:51:22 +0300

    svg logo stuff + links

--- src/views/Start.tsx
+++ src/views/Start.tsx
@@ -1,13 +1,12 @@
 import React, { FunctionComponent } from 'react';
-import moment from 'moment';
 import { NavLink, Route, Switch } from 'react-router-dom';
 import Interval from 'react-interval-rerender';
 
-import InfoItem from '../components/InfoItem';
+import Cam from '../views/Cam';
+import Friends from '../components/Friends';
 import PlayButton from '../components/PlayButton';
 import { ProgramListItem } from '../components/Program/ProgramItem';
 import { Program, getCurrentProgram } from '../logic/Program';
-import Cam from '../views/Cam';
 
 const ReactMarkdown = require('react-markdown');
 
@@ -16,28 +15,28 @@ interface StartProps {
   programs: Program[]
 }
 
-const friends = [
-    { filename: 'ambientia.png', url: 'http://www.ambientia.fi/' },
-    { filename: 'atostek.png', url: 'http://www.atostek.com/' },
-    { filename: 'bitwise.png', url: 'http://www.bitwise.fi/' },
-    { filename: 'cinia.png', url: 'http://www.cinia.fi/' },
-    { filename: 'cybercom.png', url: 'http://www.cybercom.com/' },
-    { filename: 'digia.png', url: 'http://www.digia.com/' },
-    { filename: 'etteplan.png', url: 'http://www.etteplan.com/' },
-    { filename: 'futurice.gif', url: 'http://www.futurice.com/' },
-    { filename: 'gofore.png', url: 'http://www.gofore.com/' },
-    { filename: 'intopalo.png', url: 'http://www.intopalo.com/' },
-    { filename: 'keisari.png', url: 'http://www.nokianpanimo.fi/' },
-    { filename: 'misnot.png', url: 'http://www.misnot.fi/' },
-    { filename: 'netum.png', url: 'http://www.netum.fi/' },
-    { filename: 'poas.png', url: 'http://www.poas.fi/' },
-    { filename: 'profit.png', url: 'http://www.profitsoftware.com/' },
-    { filename: 'pyynikki.png', url: 'http://www.pyynikin.com/' },
-    { filename: 'rdvelho.png', url: 'http://www.rdvelho.com/' },
-    { filename: 'solita.png', url: 'http://www.solita.fi/' },
-    { filename: 'vincit.jpg', url: 'http://www.vincit.fi/' },
-    { filename: 'wapice.png', url: 'http://www.wapice.com/' },
-    { filename: 'yit.png', url: 'http://www.yit.fi/' }]
+// const friends = [
+//     { filename: 'ambientia.png', url: 'http://www.ambientia.fi/' },
+//     { filename: 'atostek.png', url: 'http://www.atostek.com/' },
+//     { filename: 'bitwise.png', url: 'http://www.bitwise.fi/' },
+//     { filename: 'cinia.png', url: 'http://www.cinia.fi/' },
+//     { filename: 'cybercom.png', url: 'http://www.cybercom.com/' },
+//     { filename: 'digia.png', url: 'http://www.digia.com/' },
+//     { filename: 'etteplan.png', url: 'http://www.etteplan.com/' },
+//     { filename: 'futurice.gif', url: 'http://www.futurice.com/' },
+//     { filename: 'gofore.png', url: 'http://www.gofore.com/' },
+//     { filename: 'intopalo.png', url: 'http://www.intopalo.com/' },
+//     { filename: 'keisari.png', url: 'http://www.nokianpanimo.fi/' },
+//     { filename: 'misnot.png', url: 'http://www.misnot.fi/' },
+//     { filename: 'netum.png', url: 'http://www.netum.fi/' },
+//     { filename: 'poas.png', url: 'http://www.poas.fi/' },
+//     { filename: 'profit.png', url: 'http://www.profitsoftware.com/' },
+//     { filename: 'pyynikki.png', url: 'http://www.pyynikin.com/' },
+//     { filename: 'rdvelho.png', url: 'http://www.rdvelho.com/' },
+//     { filename: 'solita.png', url: 'http://www.solita.fi/' },
+//     { filename: 'vincit.jpg', url: 'http://www.vincit.fi/' },
+//     { filename: 'wapice.png', url: 'http://www.wapice.com/' },
+//     { filename: 'yit.png', url: 'http://www.yit.fi/' }]
 
 const Start: FunctionComponent<StartProps> = ({infoData, programs}) => (
   <section className='view-container -start'>
@@ -47,7 +46,7 @@ const Start: FunctionComponent<StartProps> = ({infoData, programs}) => (
       <div className='info'>
         <h2>Studiossa</h2>
         <Interval delay={60000}>
-          {() => { return <ProgramListItem {...getCurrentProgram(programs)} /> } }
+          {() => <ProgramListItem {...getCurrentProgram(programs)} />}
         </Interval>
         {/*<InfoItem
           title='Nyt soi'
@@ -72,15 +71,8 @@ const Start: FunctionComponent<StartProps> = ({infoData, programs}) => (
         <Cam url="https://mordor.wappuradio.fi/hls/wappuradio.m3u8" />
       }/>
     </Switch>
-    <div className='friends'>
-    { friends.map((friend) => {
-        var logo = 'https://wappuradio.fi/files/img/friend/'+friend.filename
-        var lol = { order: Math.floor(Math.random()*100) }
-        return (
-            <a href={friend.url} style={lol}><img src={logo} width='240' height='70' /></a>
-        );
-    }) }
-    </div>
+    <h2>Menossa mukana</h2>
+    <Friends />
     <h2>Striimilinkit</h2>
     <ul>
       <li><a href="https://wappuradio.fi/wappuradio.m3u">M3U playlist</a></li>
