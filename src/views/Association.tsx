6f467b7cfc7cdfae7655e73e739045c8420085e0
Esa Varemo <git@esav.fi>
2022-03-02 13:50:26 +0200

    Update dates and names to 2022

--- src/views/Association.tsx
+++ src/views/Association.tsx
@@ -9,17 +9,16 @@ const Association: FunctionComponent<AssociationProps> = () => (
         <h1>Wappuradion Tuki ry</h1>
         <p><strong>Wappuradion Tuki ry</strong> toimii Rakkauden Wappuradion taustalla.</p>
   <p>Sähköposti: <a href='mailto:hallitus@wappuradio.fi'>hallitus@wappuradio.fi</a></p>
-  <h2>Hallitus 2020-2021:</h2>
-  <p>Puheenjohtaja: Veli Tapper<br />
-  Varapuheenjohtaja: Helky Kouri<br />
-  Rahastonhoitaja: Jaakko Airasmaa<br />
+  <h2>Hallitus 2021-2022:</h2>
+  <p>Puheenjohtaja: Saskia Simisker<br />
+  Varapuheenjohtaja: Jaakko Airasmaa<br />
+  Rahastonhoitaja: Sami Hartala<br />
   Sihteeri: Tytti Hyvönen<br />
-  Hallituksen jäsen: Tuulia Kusmin<br />
-  Hallituksen jäsen: Hilppa Kouri<br />
-  Hallituksen jäsen: Saskia Simisker<br /></p>
-  <h2>Toimihenkilöt 2021:</h2>
-  <p>Maria Järvelin<br />
-  Tuomas Tinus</p>
+  Hallituksen jäsen: Veli Tapper<br /></p>
+  <h2>Toimihenkilöt 2022:</h2>
+  <p>Salla-Mari Palokari<br />
+  Emmi Oikkonen<br />
+  Esa Varemo</p>
     </section>
 );
 
--- src/views/Info.tsx
+++ src/views/Info.tsx
@@ -14,8 +14,8 @@ const Info: FunctionComponent<InfoProps> = ({infoData, licenseData}) => (
             Rakkauden Wappuradio reaches over 2,000 listeners over local FM and worldwide over the Internet.
             The target audience is students and young professionals. The primary language is Finnish, with intermittent international programming.
     </i></p>
-    <p><strong>Rakkauden Wappuradio&trade;</strong> lähettää ohjelmaa taas <strong>15.4.2021&nbsp;klo&nbsp;14</strong> alkaen ympäri vuorokauden aina wappuaattoon asti. Kuuntele Tampereen alueella taajuudella <strong>101,6&nbsp;MHz</strong> tai netissä. Liity siis hypetysjoukkoihin <a href='https://t.me/wappuradio' target='_blank'>Telegram</a>-ryhmässä tai IRCnetissä kanavalla <strong>#wappuradio</strong>.</p>
-    <p>Rakkauden Wappuradio sai alkunsa vuonna 2010 Tampereen Teekkarien PerinneSeuran ja Tampereen teekkarien radiokerhon yhteistyöprojektista, mutta noista päivistä tekijäjoukko on laajentunut rajusti. Wappuradiossa toimii tällä hetkellä monipuolinen tiimi, joita yhdistää kiinnostus tekniikkaan, radion tekemiseen ja Wappuun. Rakkauden Wappuradio jatkaa viimevuosien perinteitä ja tulee korvaasi radion välityksellä taas 15.4.–30.4. Välitämme Wapun ilosanomaa teekkarihengessä radiotaajuuksilla ja internetissä. Tänäkin vuonna aiomme olla maailman paras tamperelainen Wappuradio.</p>
+    <p><strong>Rakkauden Wappuradio&trade;</strong> lähettää ohjelmaa taas <strong>13.4.2022&nbsp;klo&nbsp;14</strong> alkaen ympäri vuorokauden aina wappuaattoon asti. Kuuntele Tampereen alueella taajuudella <strong>101,6&nbsp;MHz</strong> tai netissä. Liity siis hypetysjoukkoihin <a href='https://t.me/wappuradio' target='_blank'>Telegram</a>-ryhmässä tai IRCnetissä kanavalla <strong>#wappuradio</strong>.</p>
+    <p>Rakkauden Wappuradio sai alkunsa vuonna 2010 Tampereen Teekkarien PerinneSeuran ja Tampereen teekkarien radiokerhon yhteistyöprojektista, mutta noista päivistä tekijäjoukko on laajentunut rajusti. Wappuradiossa toimii tällä hetkellä monipuolinen tiimi, joita yhdistää kiinnostus tekniikkaan, radion tekemiseen ja Wappuun. Rakkauden Wappuradio jatkaa viimevuosien perinteitä ja tulee korvaasi radion välityksellä taas 13.4.–30.4. Välitämme Wapun ilosanomaa teekkarihengessä radiotaajuuksilla ja internetissä. Tänäkin vuonna aiomme olla maailman paras tamperelainen Wappuradio.</p>
     {/*infoData.trim().length > 0 && (
       <div>
         <h2>Infoa Wappuradiosta</h2>
@@ -27,7 +27,7 @@ const Info: FunctionComponent<InfoProps> = ({infoData, licenseData}) => (
     <h2>Ota yhteyttä / Contact</h2>
     <p>Onko jutut huonoja? Onko musiikki väärää? Eikö mielipidettäsi kuunnella? Voiko tyypit tehdä täydellistä frontside ollieta? Ota yhteyttä, kysy, kehu, valita.</p>
     <p>Päätoimitus / Editors: <a href='mailto:wappuradio@wappuradio.fi'>wappuradio@wappuradio.fi</a><br />Mainosmyynti / Advertisements: <a href='mailto:mainosmyynti@wappuradio.fi'>mainosmyynti@wappuradio.fi</a><br />Webmaster: <a href='mailto:webmaster@wappuradio.fi'>webmaster@wappuradio.fi</a></p>
-    <p>Päätoimitus 2021: Maria Järvelin, Saskia Simisker, Tuomas Tinus</p>
+    <p>Päätoimitus 2022: Salla-Mari Palokari, Emmi Oikkonen, Esa Varemo</p>
     <div className='licenses'>
       <h3>Lisenssit</h3>
       <p>Radiossa saatetaan käyttää seuraavia lisenssin alaisia äänitteitä:</p>
