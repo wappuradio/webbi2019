f0c6f1daeafcc2c21a047d225e3d4c9945cf3132
Ville Ranki <ville.ranki@iki.fi>
2024-03-27 20:58:59 +0200

    Käytetään wappuradion sometunnusta instanssin sijaan, nyt siellä on sisältöä

--- src/components/MastoFeed.tsx
+++ src/components/MastoFeed.tsx
@@ -6,7 +6,7 @@ const MastoFeed: FunctionComponent = () => (
     <Helmet>
         <script type="module" src="https://esm.sh/emfed@1" crossOrigin="anonymous" async></script>
     </Helmet>
-    <a className="mastodon-feed" href="https://mementomori.social/@Instanssi" data-toot-limit="4"></a>
+    <a className="mastodon-feed" href="https://mementomori.social/@wappuradio" data-toot-limit="4"></a>
     <link rel="stylesheet" type="text/css" href="/toots.css"></link>
     </section>
   );
