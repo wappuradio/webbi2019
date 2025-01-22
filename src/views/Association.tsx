db947ff9fd305660fee1b8cb7cbab6abd26bddf2
Esa Varemo <git@esav.fi>
2025-01-22 18:34:23 +0200

    Hallitus 2024-2025

--- src/views/Association.tsx
+++ src/views/Association.tsx
@@ -4,22 +4,32 @@ interface AssociationProps {
 }
 
 const Association: FunctionComponent<AssociationProps> = () => (
-    <section className='view-container'>
-        <h1>Wappuradion Tuki ry</h1>
-        <p><strong>Wappuradion Tuki ry</strong> toimii Rakkauden Wappuradion taustalla.</p>
-  <p>Sähköposti: <a href='mailto:hallitus@wappuradio.fi'>hallitus@wappuradio.fi</a></p>
-  <h2>Hallitus 2023-2024:</h2>
-  <p>Puheenjohtaja: Tuomas Tinus<br />
-  Varapuheenjohtaja: Jemina Raukamo<br />
-  Sihteeri: Maria Järvelin<br />
-  Rahastonhoitaja: Aleksi Sivonen</p>
-  <h2>Toimihenkilöt 2024:</h2>
-  <p>Raafael Juntti<br />
-  Tytti Hyvönen<br />
-  Elias Meyer<br />
-  Tuulia Kusmin<br />
-  Sanna Kallio</p>
-    </section>
+  <section className='view-container'>
+    <h1>Wappuradion Tuki ry</h1>
+    <p><strong>Wappuradion Tuki ry</strong> toimii Rakkauden Wappuradion taustalla.</p>
+    <p>Sähköposti: <a href='mailto:hallitus@wappuradio.fi'>hallitus@wappuradio.fi</a></p>
+    <h2>Hallitus 2024-2025:</h2>
+    <p>Puheenjohtaja: Elias Meyer<br />
+    Varapuheenjohtaja & Viestintähenkilö: Raafael Juntti<br />
+    Sihteeri: Tytti Hyvönen<br />
+    Rahastonhoitaja: Sanna Kallio</p>
+
+    <h2>Toimihenkilöt 2024-2025:</h2>
+    <p>
+    Vuosijuhlatoimihenkilö: Tuulia Kusmin<br />
+    Tekniikkatonttu: Esa Varemo<br />
+    </p>
+
+    <h3>Päätoimitus:</h3>
+    <p>
+    Aatos Mickelsson<br />
+    Ale Kemppainen<br />
+    Hannu Vuolasaho<br />
+    Jemina Raukamo<br />
+    Jussi Lähteenmäki
+    </p>
+  
+  </section>
 );
 
 export default Association;
