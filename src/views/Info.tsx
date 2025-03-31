976572799e05a96a39cc08eaa6e12cfd1f45de8f
Henri Sarasvirta <henri.sarasvirta@gmail.com>
2025-03-31 22:28:53 +0300

    Update info page with 2025 info.

--- src/views/Info.tsx
+++ src/views/Info.tsx
@@ -11,9 +11,9 @@ const Info: FunctionComponent<InfoProps> = ({infoData, licenseData}) => (
     <h1>Rakkauden Wappuradio</h1>
     <p><i><strong>Rakkauden Wappuradio&trade;</strong> is a student radio station based in Tampere, Finland. The station was founded in 2010 and broadcasts every year for two weeks at the end of April. Rakkauden Wappuradio reaches over 2,000 listeners over local FM and worldwide over the Internet. The target audience is students and young professionals. The primary language is Finnish, with intermittent international programming.
     </i></p>
-    <p><strong>Rakkauden Wappuradio&trade;</strong> lähettää ohjelmaa taas <strong>11.4.2024&nbsp;klo&nbsp;14</strong> alkaen ympäri vuorokauden aina wappuaattoon asti. Kuuntele Tampereen alueella taajuudella <strong>106,4&nbsp;MHz</strong> tai netissä. Liity siis hypetysjoukkoihin <a href='https://t.me/wappuradio' target='_blank' rel="noreferrer">Telegram</a>-ryhmässä tai IRCnetissä kanavalla <strong>#wappuradio</strong>.
+    <p><strong>Rakkauden Wappuradio&trade;</strong> lähettää ohjelmaa taas <strong>14.4.2025&nbsp;klo&nbsp;14</strong> alkaen ympäri vuorokauden aina wappuaattoon asti. Kuuntele Tampereen alueella taajuudella <strong>101,6&nbsp;MHz</strong> tai netissä. Liity siis hypetysjoukkoihin <a href='https://t.me/wappuradio' target='_blank' rel="noreferrer">Telegram</a>-ryhmässä tai IRCnetissä kanavalla <strong>#wappuradio</strong>.
     </p>
-    <p>Rakkauden Wappuradio sai alkunsa vuonna 2010 Tampereen Teekkarien PerinneSeuran ja Tampereen teekkarien radiokerhon yhteistyöprojektista, mutta noista päivistä tekijäjoukko on laajentunut rajusti. Wappuradiossa toimii tällä hetkellä monipuolinen tiimi, joita yhdistää kiinnostus tekniikkaan, radion tekemiseen ja Wappuun. Rakkauden Wappuradio jatkaa viimevuosien perinteitä ja tulee korvaasi radion välityksellä taas 11.4.–30.4. Välitämme Wapun ilosanomaa teekkarihengessä radiotaajuuksilla ja internetissä. Tänäkin vuonna aiomme olla maailman paras tamperelainen Wappuradio.
+    <p>Rakkauden Wappuradio sai alkunsa vuonna 2010 Tampereen Teekkarien PerinneSeuran ja Tampereen teekkarien radiokerhon yhteistyöprojektista, mutta noista päivistä tekijäjoukko on laajentunut rajusti. Wappuradiossa toimii tällä hetkellä monipuolinen tiimi, joita yhdistää kiinnostus tekniikkaan, radion tekemiseen ja Wappuun. Rakkauden Wappuradio jatkaa viimevuosien perinteitä ja tulee korvaasi radion välityksellä taas 14.4.–30.4. Välitämme Wapun ilosanomaa teekkarihengessä radiotaajuuksilla ja internetissä. Tänäkin vuonna aiomme olla maailman paras tamperelainen Wappuradio.
     </p>
     {/*infoData.trim().length > 0 && (
       <div>
@@ -28,7 +28,7 @@ const Info: FunctionComponent<InfoProps> = ({infoData, licenseData}) => (
     <p>Päätoimitus / Editors: <a href='mailto:wappuradio@wappuradio.fi'>wappuradio@wappuradio.fi</a><br />
             Mainosmyynti / Advertisements: <a href='mailto:mainosmyynti@wappuradio.fi'>mainosmyynti@wappuradio.fi</a><br />
             Webmaster: <a href='mailto:webmaster@wappuradio.fi'>webmaster@wappuradio.fi</a></p>
-    <p>Päätoimitus 2024: Tytti Hyvönen, Tuulia Kusmin, Sanna Kallio, Elias Meyer, Raafael Juntti</p>
+    <p>Päätoimitus 2025: Aatos Mickelsson, Ale Kemppainen, Hannu Vuolasaho, Jemina Raukamo, Jussi Lähteenmäki</p>
     <div className='licenses'>
       <h3>Lisenssit</h3>
       <p>Radiossa saatetaan käyttää seuraavia lisenssin alaisia äänitteitä:</p>
