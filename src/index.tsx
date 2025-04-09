9039efa402d37a1a2952496d5db5fbf0469be2e0
Petri Sarasvirta <petri.sarasvirta@gofore.com>
2025-04-10 01:03:22 +0300

    craco removed, obsolete node ssas removed & working local development with webpack 5 & react HMR

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createRoot} from 'react-dom/client';
 
 import './style/style.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 import './hls.d.ts';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+const container = document.getElementById('root');
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const root = createRoot(container!);
+root.render(<App />);
 
 serviceWorker.unregister();
