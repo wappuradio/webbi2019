d9733a1507139bfc29fad7795fd84808a726f312
Esa Varemo <git@esav.fi>
2026-01-05 19:42:50 +0200

    hallitus/pt 2025-2026

--- src/views/Association.tsx
+++ src/views/Association.tsx
@@ -8,28 +8,29 @@ const Association: FunctionComponent<AssociationProps> = () => (
     <h1>Wappuradion Tuki ry</h1>
     <p><strong>Wappuradion Tuki ry</strong> toimii Rakkauden Wappuradion taustalla.</p>
     <p>Sähköposti: <a href='mailto:hallitus@wappuradio.fi'>hallitus@wappuradio.fi</a></p>
-    <h2>Hallitus 2024-2025:</h2>
-    <p>Puheenjohtaja: Elias Meyer<br />
-    Varapuheenjohtaja & Viestintähenkilö: Raafael Juntti<br />
-    Sihteeri: Tytti Hyvönen<br />
-    Rahastonhoitaja: Sanna Kallio</p>
+    <h2>Hallitus 2025-2026:</h2>
+    <p>Puheenjohtaja: Raafael Juntti<br />
+    Sihteeri & Varapuheenjohtaja: Tytti Hyvönen<br />
+    Rahastonhoitaja: Sanna Kallio<br />
+    Viestintävastaava: Jemina Raukamo<br />
+    Hallituksen jäsen: Hannu Vuolasaho<br />
+    Hallituksen jäsen: Joni Selänne</p>
 
-    <h2>Toimihenkilöt 2024-2025:</h2>
+    <h2>Toimihenkilöt 2025-2026:</h2>
     <p>
-    Vuosijuhlatoimihenkilö: Tuulia Kusmin<br />
     Tekniikkatonttu: Esa Varemo<br />
     </p>
 
     <h3>Päätoimitus:</h3>
     <p>
+    Alexander Lindholm<br />
+    Paju Tella<br />
     Aatos Mickelsson<br />
-    Ale Kemppainen<br />
-    Hannu Vuolasaho<br />
-    Jemina Raukamo<br />
-    Jussi Lähteenmäki
+    Lassi Alm
     </p>
   
   </section>
 );
 
 export default Association;
+
