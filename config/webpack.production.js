1110c859137bee27bb3d81a8a9fc9ad6bb352662
Taavi Väänänen <taavi@majava.org>
2026-04-03 23:32:51 +0300

    Generate separate CSS files

--- config/webpack.production.js
+++ config/webpack.production.js
@@ -1,12 +1,17 @@
 const path = require("path");
-const rootPath = path.resolve(__dirname, "../");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { merge } = require('webpack-merge');
+
+const rootPath = path.resolve(__dirname, "../");
 const common = require('./webpack.common');
 
 const prodConfig = {
     output: {
         path: path.resolve(rootPath, 'build'),
-    }
+    },
+    plugins: [
+        new MiniCssExtractPlugin(),
+    ],
 }
 
 const merged = merge(common, prodConfig)
