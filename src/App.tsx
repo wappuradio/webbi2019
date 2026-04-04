a4a8bc5b95e029a13cdb35a77207f8bde9beb2c9
Taavi Väänänen <taavi@majava.org>
2026-04-04 13:59:42 +0300

    Do not include Moment in the initial bundle
    Moment.js and all of its locales are massive. So only load those
    libraries in the bundle that contains those libraries instead of loading
    it for the spinner.
    
    One day this all will hopefully use Temporal instead..

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
 import React, { FunctionComponent } from 'react';
 import Loadable from 'react-loadable';
 
-// set moment locale
-import moment from 'moment';
-import 'moment/locale/fi';
 import Spinner from './components/Spinner';
-moment.locale('fi');
 
 const APIWrapper = Loadable({
   loader: () => import('./components/APIWrapper'),
