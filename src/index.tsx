7bd6de5c0d2e9ca68882b38f06d983c3e765dbc6
Taavi Väänänen <taavi@majava.org>
2026-04-04 13:55:18 +0300

    Remove unused service worker logic

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,8 @@ import {createRoot} from 'react-dom/client';
 
 import './style/style.scss';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 const container = document.getElementById('root');
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const root = createRoot(container!);
 root.render(<App />);
-
-serviceWorker.unregister();
