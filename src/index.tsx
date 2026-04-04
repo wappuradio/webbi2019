8e1dfcba25989f846b3d9752529d28c6c5b1eb96
Taavi Väänänen <taavi@majava.org>
2026-04-04 13:54:41 +0300

    Remove unused typescript stuff

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,6 @@ import './style/style.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import './hls.d.ts';
 const container = document.getElementById('root');
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const root = createRoot(container!);
