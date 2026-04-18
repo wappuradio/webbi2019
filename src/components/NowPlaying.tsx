ff4fecfe039ec1946c1cbd5ec1cc7624aeb0e069
Taavi Väänänen <taavi@majava.org>
2026-04-18 20:46:29 +0300

    NowPlaying: Convert to a functional component

--- src/components/NowPlaying.tsx
+++ src/components/NowPlaying.tsx
@@ -1,26 +1,31 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import socketIOClient from 'socket.io-client';
 import InfoItem from './InfoItem';
 
-class NowPlaying extends Component {
-  state = {
-    artist: '',
-    title: ''
-  }
-  componentDidMount = () => {
-    const socket = socketIOClient('https://wappuradio.fi/');
-    socket.on('np', (song => {
-      song = song.song.split(' - ');
-      var artist = song[0] || '';
-      var title = song[1] || '';
-      this.setState({artist: artist, title: title});
-    }));
-  }
-  render() {
-    return (
-      <InfoItem title='Nyt Soi' content={this.state.title} subcontent={this.state.artist} />
-    )
-  }
-}
+const socket = socketIOClient('https://wappuradio.fi/');
+
+type Song = { artist: string; title: string; };
+type SongMessage = { song: string; };
+
+const NowPlaying: React.FC<{}> = () => {
+  const [song, setSong] = useState<Song>({ artist: '', title: '' });
+  useEffect(() => {
+    const onNp = (song: SongMessage) => {
+      const parts = song.song.split(' - ');
+      var artist = parts[0] || '';
+      var title = parts[1] || '';
+
+      setSong({ artist, title });
+    };
+
+    socket.on('np', onNp);
+
+    return () => {
+      socket.off('np', onNp);
+    };
+  });
+
+  return <InfoItem title='Nyt Soi' content={song.title} subcontent={song.artist} />;
+};
 
 export default NowPlaying;
