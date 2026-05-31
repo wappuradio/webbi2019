544c5221e0be6b71ca4388521fb8f9d89fe0af3a
zertap <touch@fluffytail.me>
2026-05-31 16:36:00 +0300

    socket.io: disable credential sending

--- src/components/NowPlaying.tsx
+++ src/components/NowPlaying.tsx
@@ -6,9 +6,11 @@ const IS_DEV = ['webbi-beta.wappuradio.fi', 'webbi-test.wappuradio.fi'].includes
 const SOCKET_IO_PATH = IS_DEV ? '/test/api/socket.io' : '/socket.io';
 
 const socket = socketIOClient(
-  withCredentials: false,
   'https://wappuradio.fi/',
-  { path: SOCKET_IO_PATH }
+  {
+    withCredentials: false,
+    path: SOCKET_IO_PATH
+  }
 );
 
 type Song = { artist: string; title: string; };
